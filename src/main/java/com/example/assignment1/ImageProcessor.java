11216831a22e32911227d15ea03c1b777767a2b2
calc6 <calum.cullen66@gmail.com>
2024-02-12 14:52:10 +0000

    New Method

--- src/main/java/com/example/assignment1/ImageProcessor.java
+++ src/main/java/com/example/assignment1/ImageProcessor.java
@@ -3,6 +3,27 @@ package com.example.assignment1;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class ImageProcessor {}
+public class ImageProcessor {
+
+
+    public static BufferedImage convertToBlackAndWhite(BufferedImage originalImage, Color targetColor, double threshold) {
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+        BufferedImage bwImage = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_BINARY);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                Color pixelColor = new Color(originalImage.getRGB(x, y));
+                if (colorSimilarity(pixelColor, targetColor) < threshold) {
+                    bwImage.setRGB(x, y, Color.WHITE.getRGB());
+                } else {
+                    bwImage.setRGB(x, y, Color.BLACK.getRGB());
+                }
+            }
+        }
+        return bwImage;
+    }
+
+}
 
 
